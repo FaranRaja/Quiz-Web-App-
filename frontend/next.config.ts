86aa380c2b0e068ff48738c1cb287a746916f4f0
Faran Raheel Raja <145844654+FaranRaja@users.noreply.github.com>
2024-11-29 18:55:04 +0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds = true,
+  },
   typescript: {
     // Ensures the TypeScript build is included in the Vercel deployment
     ignoreBuildErrors: false, // You can set to true if you want to bypass TypeScript errors
